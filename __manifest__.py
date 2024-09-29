4fc1a2566e8c3f1d12f9b416bc97ed64a265f988
Bui Huu Loc <huuloc.bui@linnoedge.com>
2024-09-29 20:43:01 +0700

    feat: add perm

--- __manifest__.py
+++ __manifest__.py
@@ -24,7 +24,7 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/user_management_views.xml',
         'views/user_management_menu.xml',
         'views/course_management_views.xml',
